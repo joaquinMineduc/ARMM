00f8c5dd7ab0873a192e59a8aa234d5783a961bc
joaquin-mineduc <joaquin.astorga@mineduc.cl>
2024-12-10 16:03:48 -0300

    avance 10-12-24

--- Backend/eval_functions.py
+++ Backend/eval_functions.py
@@ -2,7 +2,7 @@ from principal_functions import *
 
 
 # =============== Tratamiento DF regional ==================================
-df_regional = create_dataframe("Backend/Input/Eval_internal/eval.xls",
+df_regional = create_dataframe("APP/Backend/Input/Eval_internal/eval.xls",
                             'Evaluación Interna por Región', 2)
 
 df_regional = drop_unless_columns(df_regional, 1, 7, None)
@@ -17,7 +17,7 @@ format_eval_columns(df_regional)
 print(df_regional)
 
 # ================= Tratamiento DF NC =======================================
-df_NC = create_dataframe('Backend/Input/Eval_internal/eval.xls',
+df_NC = create_dataframe('APP/Backend/Input/Eval_internal/eval.xls',
                          'Evaluación Interna por División', 3)
 
 df_NC = drop_unless_columns(df_NC, 1, 7, None)
@@ -32,15 +32,57 @@ df_NC_part4 = partioner(df_NC, 7, 8)
 
 # =========== Tratamiento DF NC por responsable ===================
 
-df_NC2 = create_dataframe('Backend/Input/Eval_internal/eval.xls',
+df_NC2 = create_dataframe('APP/Backend/Input/Eval_internal/eval.xls',
                           'Eval. interna por responsable', 3)
 
+df_NC2 = drop_unless_columns(df_NC2, 2, 12, None) # Se elimina antes para evitar el resize de los index
 df_NC2 = drop_unless_columns(df_NC2, None, None, 0) # Se elimina porque este valor esta intercalado, es necesario eliminar por separado
-df_NC2 = drop_unless_columns(df_NC2, 1, 12, None)
 
 #Se particiona el dataframe eval interna por responsables
 df_NC2_part1 = partioner(df_NC2, 3, 10)
 df_NC2_part2 = partioner(df_NC2, 10, 13)
 df_NC2_part3 = partioner(df_NC2, 13, 17)
 
-print(df_NC2_part1)
+df_NC2_part1 = union_by_column(df_NC2_part1, 'Lugar de medición')
+df_NC2_part2 = union_by_column(df_NC2_part2, 'Lugar de medición')
+df_NC2_part3 = union_by_column(df_NC2_part3, 'Lugar de medición')
+df_NC2_part3.drop(index = 1, inplace = True)
+
+
+df_NC3 = create_dataframe('APP/Backend/Input/Eval_internal/eval.xls',
+                          "Eval. interna por variable", 2)
+
+df_NC3 = create_query(df_NC3, '[V21')
+df_NC3 = drop_unless_columns(df_NC3, None, None, [0,2,3,4,5,6,7,8,12])
+df_NC3 = modify_eval_values(df_NC3)
+df_NC3 = create_means(df_NC3, 'Género')
+
+
+df_NC4 = create_dataframe('APP/Backend/Input/Eval_internal/eval.xls',
+                          "Eval. interna por variable", 2)
+
+df_NC4 = create_query(df_NC4, '[I24')
+df_NC4 = drop_unless_columns(df_NC4, None, None, [0,2,3,4,5,6,7,8,12])
+df_NC4 = modify_eval_values(df_NC4)
+df_NC4 = create_means(df_NC4, 'Gabinete Subsecretaría')
+
+
+
+# Se remodifican los headers
+df_NC3 = df_NC3[NC_columns]
+df_NC3.columns = df_NC_columns
+
+# Se remodifican los headers
+df_NC4 = df_NC4[NC_columns]
+df_NC4.columns = df_NC_columns
+
+df_eval_NC = pd.concat([df_NC_part1, df_NC2_part1,
+                        df_NC_part2, df_NC2_part2,
+                        df_NC_part3, df_NC3,df_NC4,
+                        df_NC2_part3], axis = 0)
+
+df_eval_NC.reset_index(drop=True, inplace=True)
+format_divition(df_eval_NC)
+format(df_eval_NC)
+
+df_eval_NC.to_excel("Eval. internal.xlsx", index = False)
--- Backend/principal_functions.py
+++ Backend/principal_functions.py
@@ -81,3 +81,82 @@ def format_eval_columns(df):
 # Funcion que crea una particion de un DF
 def partioner(df, start, end):
     return df.iloc[start:end]
+
+
+# Funcion que agrupa datos según una columna
+def union_by_column(df, column):
+    df = df.groupby(by=[column]).mean()
+    df.reset_index(inplace = True)
+    df = df[order_columns_NC]
+    df.columns = df_NC_columns
+    return df
+
+#Funcion para crear consultas query
+def create_query(df, arg):
+    df = df.query(f"`Lugar de medición` == 'GABSUB' and Variable.str.startswith('{arg}')").reset_index(drop=True)
+    return df 
+
+
+def modify_eval_values(df):
+    df['Oportunidad'] = df['Oportunidad'].apply(
+        lambda op: 100 if op == 'Si' else 0 if op == 'No' else 'Sin informar')
+    df['Consistencia'] = df['Consistencia'].apply(
+        lambda op: 100 if op == 'Si' else 0 if op == 'No' else 'Sin informar')
+    df['Completitud'] = df['Completitud'].apply(
+        lambda op: 100 if op == 'Si' else 0 if op == 'No' else 'Sin informar')
+    return df
+
+# recibe como argumento dataframe y arg = nuevo valor del lugar de medición
+def create_means(df, arg):
+    columns = df.columns
+    for col in columns:
+        if isinstance(df[col].dtype, int):
+            df.loc[:, 'promedio'] = df.iloc[:,1:].mean(axis = 1)
+            df = union_by_column(df, 'Lugar de medición')
+        else:
+            df.drop_duplicates(inplace = True)
+            df.loc[:,'promedio'] = 'Sin informar'
+        df.loc[:,'Lugar de medición'] = arg
+    df.reset_index(inplace=True, drop = True)
+    return df
+
+def format_divition(df):
+    list_divition = []
+    for dv in df['División']:
+        if dv == 'RECFIN':
+            list_divition.append('Recursos Financieros')
+        elif dv == 'SUBV':
+            list_divition.append('Subvenciones')
+        elif dv == 'AYUMIN':
+            list_divition.append('Ayuda Mineduc')
+        elif dv == 'INNOV':
+            list_divition.append('Innovación')
+        elif dv == 'SEJEC_TP':
+            list_divition.append('TP')
+        elif dv in ['AUDITORIA','ESTUDIOS']:
+            list_divition.append(str(dv).lower().capitalize())
+        else:
+            list_divition.append(dv)
+    df['División'] = list_divition
+    return df
+
+
+def format_percentage(value):
+  if isinstance(value, float):
+    value = np.floor(value*10)/10
+    value = int(value)
+  value = str(value) + "%"
+  return value
+
+
+def format(df):
+  for col in df:
+    if col != 'División':
+      list_val = []
+      for row in df[col]:
+        if isinstance(row, float) or isinstance(row, int):
+          row = format_percentage(row)
+          list_val.append(row)
+        else:
+          list_val.append(row)
+      df[col] = list_val
