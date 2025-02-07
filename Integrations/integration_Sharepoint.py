feaf9cd68d0fb50a980524216d18a8a54542adbb
joaquin-mineduc <joaquin.astorga@mineduc.cl>
2025-02-07 17:09:44 -0300

    avance 07-02-2025

--- Integrations/integration_Sharepoint.py
+++ Integrations/integration_Sharepoint.py
@@ -1,9 +1,9 @@
 from scrapy_functions import *
 from selenium.webdriver.chrome.options import Options
 from pywinauto import Application
-import pyautogui
-
-
+import os
+from pathlib import Path
+from pywinauto.keyboard import send_keys
 
 def get_instruments_files():
     # Inicio sesión en Sharepoint y descarga documento ADP
@@ -37,8 +37,10 @@ def merge_and_upload_reportBI():
 #upload_reportBI()
 
 def de_prueba():
-    ruta_archivo = r"C:\Users\joaquin.astorga\mis_proyectos\Proyecto ARMM"
-    nombre_archivo = "datosBI_diciembre.xlsx"
+ 
+    ruta_archivo = "C:/Users/joaquin.astorga/mis_proyectos/Proyecto ARMM"
+    name_file = "datosBI_diciembre.xlsx"
+  
     driver = select_browser_driver()
     driver = create_new_conecction(driver,"https://shorturl.at/FsTEF")
     driver = log_in_sharepoint(driver, "armm.dpcg.system@mineduc.cl", "ERMC$7835*$")
@@ -59,7 +61,38 @@ def de_prueba():
     wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[data-automationid="uploadCommand"]'))).click()
     time.sleep(2)
     wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[data-automationid="uploadFileCommand"]'))).click()
-    time.sleep(2)
+    time.sleep(5)
 
+        # Connect to the Open File dialog
+    app = Application().connect(title_re=".*Abrir.*")
+    window = app.window(title_re=".*Abrir.*")
+
+    # Locate the toolbar where the path is shown
+    toolbar = window.child_window(title_re="Dirección:.*", class_name="ToolbarWindow32")
+    # Click the toolbar
+    toolbar.click_input()
+
+    # Select all text (Ctrl + A) and delete
+    send_keys("^a{DEL}")
+
+    # Type the new path
+    send_keys(ruta_archivo.replace(" ", "{SPACE}"))
+
+    # Press Enter
+    send_keys("{ENTER}")
+    
+    file_name_input = window.child_window(class_name="Edit", found_index=0)  
+    file_name_input.set_text(name_file)
+    time.sleep(2)
+    open_button = window.child_window(class_name="Button", found_index=0)
+    open_button.click()
+    
+    time.sleep(10)
+    
 
 de_prueba()
+
+
+
+
+
