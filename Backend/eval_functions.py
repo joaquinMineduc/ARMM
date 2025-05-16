7c2e7abb07835e07e659a8393b7f820931877e64
joaquin-mineduc <joaquin.astorga@mineduc.cl>
2025-05-16 17:10:10 -0400

    se añade nuevo formato para indicadores ptr

--- Backend/eval_functions.py
+++ Backend/eval_functions.py
@@ -43,13 +43,46 @@ df_eval_NC.loc[:,'Cumpl. promedio'] = np.mean(df_eval_NC[['Oportunidad','Consist
 
 df_eval_NC = df_eval_NC[df_NC_columns]
 
-print(df_eval_NC)
+#print(df_eval_NC)
         
 df_eval_NC = format_divition(df_eval_NC)
      
 format(df_eval_NC)
 
-print(df_eval_NC)
+#print(df_eval_NC)
 
-#df_eval_NC.to_excel("Eval. internal.xlsx", index = False)
+df_eval_NC.to_excel("Eval. internal.xlsx", index = False)
+
+
+# ===== Creación de dataframe para evaluación proveedores indicadores PTR ============
+df_NC_ptr = create_dataframe('APP/Backend/Input/Sigemet/eval.xls',
+    'Eval. interna por variable', 2)
+
+df_NC_ptr = df_NC_ptr.query("Responsable.str.contains('PTR')").copy()
+
+# =========== Tratamiento DF Evaluación proveedor interna de NC por variable para indicadores PTR ===================
+df_eval_NC_ptr = pd.DataFrame()
+for cr in cr_eval:
+    if cr in ['DIPLAP', 'GABMIN', 'GABSUB']:
+        df_temp = create_simple_query(df_NC_ptr, 'División', cr, ['División','Oportunidad',
+            'Consistencia','Completitud'])
+        df_sub_temp = create_simple_query(df_NC_ptr,'División', cr, 
+            ['Lugar de medición','Oportunidad','Consistencia','Completitud'])
+        df_unificated = build_df_eval_prov(df_temp, df_sub_temp)
+        df_eval_NC_ptr = pd.concat([df_eval_NC_ptr, df_unificated], axis = 0)
+    else:
+        df_temp = create_simple_query(df_NC_ptr, 'División', cr, ['División','Oportunidad',
+            'Consistencia','Completitud'])
+        df_unificated = build_df_eval_prov(df_temp)
+        df_eval_NC_ptr = pd.concat([df_eval_NC_ptr, df_unificated], axis = 0)
+
+df_eval_NC_ptr.loc[:,'Cumpl. promedio'] = np.mean(df_eval_NC_ptr[['Oportunidad','Consistencia','Completitud']].values, axis = 1)
+
+df_eval_NC_ptr = df_eval_NC_ptr[df_NC_columns] 
+  
+df_eval_NC_ptr = format_divition(df_eval_NC_ptr)
+     
+format(df_eval_NC_ptr)
+
+print(df_eval_NC_ptr)
 
